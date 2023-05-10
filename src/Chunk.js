437272a76b7f42d129e9b8f95b1ff0d224b32a0d
Sam Aardema <samaardema@sijthoffmedia.nl>
2023-05-10 18:12:59 +0200

    Formatting

--- src/Chunk.js
+++ src/Chunk.js
@@ -6,7 +6,11 @@ class Chunk {
     constructor(chunk_x, chunk_z, config) {
         this.chunk_x = chunk_x
         this.chunk_z = chunk_z
-        this.worldPosition = new Vector3(chunk_x * config.scale, 0, chunk_z * config.scale)
+        this.worldPosition = new Vector3(
+            chunk_x * config.scale,
+            0,
+            chunk_z * config.scale
+        )
         this.config = config
         this.noiseGenerators = []
         this.createNoiseGenerators(this.config.noise.maxIterations)
@@ -14,8 +18,8 @@ class Chunk {
         // Build base geometry with 1 vertex wide border
         // to allow edge normals to be calculated for proper seams
         this.geometry = new THREE.PlaneBufferGeometry(
-            this.config.scale + this.config.scale / this.config.detail * 2,
-            this.config.scale + this.config.scale / this.config.detail * 2,
+            this.config.scale + (this.config.scale / this.config.detail) * 2,
+            this.config.scale + (this.config.scale / this.config.detail) * 2,
             this.config.detail + 2,
             this.config.detail + 2
         )
@@ -31,7 +35,7 @@ class Chunk {
     }
 
     static material = new THREE.MeshStandardMaterial({
-        color: '#FFF',
+        color: '#FFF'
         // wireframe: true,
     })
 
@@ -42,7 +46,6 @@ class Chunk {
     build() {
         const index = this.geometry.getIndex()
         const posAttr = this.geometry.getAttribute('position')
-        const normAttr = this.geometry.getAttribute('normal')
 
         // Positions
         for (let i = 0; i < posAttr.array.length; i += 3) {
@@ -60,16 +63,25 @@ class Chunk {
             const borderIndexArray = []
 
             for (let i = 0; i < index.count; i += 3) {
-                const vA = index.getX(i);
-                const vB = index.getX(i + 1);
-                const vC = index.getX(i + 2);
+                const vA = index.getX(i)
+                const vB = index.getX(i + 1)
+                const vC = index.getX(i + 2)
                 const pA = new Vector3().fromBufferAttribute(posAttr, vA)
                 const pB = new Vector3().fromBufferAttribute(posAttr, vB)
                 const pC = new Vector3().fromBufferAttribute(posAttr, vC)
 
                 // Separate border triangles so they can be hidden
                 const s = this.config.scale
-                if (pC.x < 0 || pC.z < 0 || pA.x < 0 || pA.z < 0 || pC.x > s || pC.z > s || pB.x > s || pB.z > s) {
+                if (
+                    pC.x < 0 ||
+                    pC.z < 0 ||
+                    pA.x < 0 ||
+                    pA.z < 0 ||
+                    pC.x > s ||
+                    pC.z > s ||
+                    pB.x > s ||
+                    pB.z > s
+                ) {
                     borderIndexArray.push(vA)
                     borderIndexArray.push(vB)
                     borderIndexArray.push(vC)
@@ -108,8 +120,8 @@ class Chunk {
         let freq = this.config.noise.baseFrequency
 
         for (let i = 0; i < this.config.noise.iterations; i++) {
-            const x2 = x / this.config.scale * freq
-            const z2 = (z + this.config.noise.offset) / this.config.scale * freq
+            const x2 = (x / this.config.scale) * freq
+            const z2 = ((z + this.config.noise.offset) / this.config.scale) * freq
 
             h += this.noiseGenerators[i].perlin2(x2, z2) * scale
             freq *= this.config.noise.freqMult
